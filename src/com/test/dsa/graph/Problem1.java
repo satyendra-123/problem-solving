54a109362d410fc1415af97d280b2fcb2328f1b7
Satyendra Singh Kotiya <satyendra.kotiya@thoughtworks.com>
2023-01-16 00:01:37 +0530

    ps|adding problem statement for union find|satyendra

---
+++ src/com/test/dsa/graph/Problem1.java
@@ -0,0 +1,28 @@
+package com.test.dsa.graph;
+
+/***
+ * You are given two strings of the same length s1 and s2 and a string baseStr.
+ *
+ * We say s1[i] and s2[i] are equivalent characters.
+ *
+ * For example, if s1 = "abc" and s2 = "cde", then we have 'a' == 'c', 'b' == 'd', and 'c' == 'e'.
+ * Equivalent characters follow the usual rules of any equivalence relation:
+ *
+ * Reflexivity: 'a' == 'a'.
+ * Symmetry: 'a' == 'b' implies 'b' == 'a'.
+ * Transitivity: 'a' == 'b' and 'b' == 'c' implies 'a' == 'c'.
+ * For example, given the equivalency information from s1 = "abc" and s2 = "cde", "acd" and "aab" are equivalent strings of baseStr = "eed", and "aab" is the lexicographically smallest equivalent string of baseStr.
+ *
+ *
+ *
+ * Return the lexicographically smallest equivalent string of baseStr by using the equivalency information from s1 and s2.
+ *
+ * Input: s1 = "parker", s2 = "morris", baseStr = "parser"
+ * Output: "makkek"
+ * Explanation: Based on the equivalency information in s1 and s2, we can group their characters as [m,p], [a,o], [k,r,s], [e,i].
+ * The characters in each group are equivalent and sorted in lexicographical order.
+ * So the answer is "makkek".
+ */
+public class Problem1 {
+
+}
